4077cd8f2c81a96b53caa7f105cc04b10cd1c5f8
Laxman Khatri <137501227+melaxman@users.noreply.github.com>
2024-05-28 00:36:29 +0545

    Add files via upload

---
+++ User.java
@@ -0,0 +1,19 @@
+public class User {
+    String email,pass;
+    public void setEmail(String email)
+    {
+        this.email=email;
+
+
+    }
+    public void setPass(String pass){
+        this.pass=pass;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public String getPass(){
+        return this.pass;
+    }
+}
+
